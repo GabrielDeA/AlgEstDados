146fd855a990dcd9842e95ad81770d76cfed6ff6
GabrielDeA <102760634+GabrielDeA@users.noreply.github.com>
2023-03-28 10:19:07 -0300

    Update pilhaLista.java

--- pilhaLista.java
+++ pilhaLista.java
@@ -28,7 +28,7 @@ public class pilhaLista<T> implements Pilha<T>{
         if(EstaVazia() == true) {
             throw new RuntimeException("Pilha está vazia");
         } else {
-            return lista.primeiro.info;
+            return lista.ultimo.info;
         }
     }
 
