17e19faf15e40243d5ef56be34b3d982dbb3f12a
GabrielDeA <102760634+GabrielDeA@users.noreply.github.com>
2023-05-30 17:07:51 -0300

    Update MapaDispersao.java

--- Trabalho3/MapaDispersao.java
+++ Trabalho3/MapaDispersao.java
@@ -1,10 +1,36 @@
-package Trabalho3;
-
 public class MapaDispersao<K, T> {
-    public Lista<K, T> tabela;
+    public ListaEncadeada<K, T> tabela[];
 
     public void MapaDispersao(int quantidade) {
-        this.tabela = new ListaEstatica(quantidade);
+        this.tabela = new ListaEncadeada[quantidade];
+    }
+
+    public boolean inserir(K chave, T dado) {
+        int indice = this.calcularHash(chave);
+
+        if(tabela[indice] == null) {
+            tabela[indice].inserir(dado);
+            return true;
+        } else {
+            NoLista<K, T> no = tabela[indice].primeiro;
+            while(no.getProx() != null) {
+                if (no.chave.equals(chave)) {
+                    no.info = dado;
+                    return true;
+                }
+                no = no.getProx();
+            } 
+            
+        }
+        return false;
+    }
+
+    private int calcularHash(K chave) {
+        int hash = chave.hashCode();
+
+        int compactado = hash % this.tabela.length;
+        
+        return compactado;
     }
 
 }
